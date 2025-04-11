7c4a9396c395b8edb3c4a4f2a9c59afd21d51402
queren-somarriba <qsomarri@made-f0Dr7s12.clusters.42paris.fr>
2025-04-11 19:56:53 +0200

    fix ray_vect

--- srcs/display/ray.c
+++ srcs/display/ray.c
@@ -6,7 +6,7 @@
 /*   By: qsomarri <qsomarri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 16:17:38 by qsomarri          #+#    #+#             */
-/*   Updated: 2025/04/11 17:44:39 by qsomarri         ###   ########.fr       */
+/*   Updated: 2025/04/11 19:50:27 by qsomarri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,25 @@
 
 t_vector	make_vect_dir(t_minirt *data, float x, float y)
 {
-	t_vector	v_dir;
-	float		a;
-	float		b;
-	float		c;
+	t_vector	forw;
+	t_vector	right;
+	t_vector	up;
+	float		f[4];
+	t_vector	v_cam;
 
-	a = x + 0.5 - WIN_WIDTH * 0.5;
-	b = y + 0.5 - WIN_HEIGHT * 0.5;
-	c = WIN_HEIGHT;
-	if (WIN_WIDTH > WIN_HEIGHT)
-		c = WIN_WIDTH;
-	c /= (2 * tanf((data->cam->fov * 0.5) * M_PI / 180.0));
-	v_dir = (t_vector){a, b, c};
-	return (normalize_vect(v_dir));
+	f[0] = (float)WIN_WIDTH / WIN_HEIGHT;
+	f[1] = tanf((data->cam->fov * 0.5) * M_PI / 180.0);
+	f[2] = (2 * (x + 0.5) / WIN_WIDTH - 1) * f[0] * f[1];
+	f[3] = (1 - 2 * (y + 0.5) / WIN_HEIGHT) * f[1];
+	forw = normalize_vect(*data->cam->v);
+	if (fabsf(forw.x) == 0 && fabsf(forw.z) == 0)
+		right = (t_vector){1, 0, 0};
+	else
+		right = normalize_vect(mult_vect((t_vector){0, 1, 0}, forw));
+	up = mult_vect(forw, right);
+	v_cam = add_vector(add_vector(vector_scale(right, f[2]),
+			vector_scale(up, f[3])), forw);
+	return (normalize_vect(v_cam));
 }
 
 void	ray_tracer(t_minirt *data)
