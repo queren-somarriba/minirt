8bbf332249eeeb6f30fdb808f7e1eee36494922d
Jordan Piech <jordan.piech@gmail.com>
2025-05-26 18:11:45 +0200

    retrait de (void)c dans diffuse reflexion

--- srcs/display/color.c
+++ srcs/display/color.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   color.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qsomarri <qsomarri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jpiech <jpiech@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 13:13:14 by qsomarri          #+#    #+#             */
-/*   Updated: 2025/04/25 19:12:53 by qsomarri         ###   ########.fr       */
+/*   Updated: 2025/05/26 18:10:40 by jpiech           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ t_color	diffuse_reflexion(t_inter *i, t_light *l, t_color c, t_vector to_light)
 	t_color		diffuse;
 	float		x;
 
-	(void)c;
 	to_light = normalize_vect(to_light);
 	x = dot_product(i->normal, to_light);
 	if (x < 0.0f)
@@ -56,6 +55,6 @@ t_color	final_color(t_minirt *data, t_inter *inter)
 		data->light->p->y - inter->p.y, data->light->p->z - inter->p.z};
 	final = add_color(inter->c, *(data->amb->color), data->amb->ratio);
 	if (!is_in_shadow(data, inter, to_light))
-		final = diffuse_reflexion(inter, data->light, final, to_light);
+	 	final = diffuse_reflexion(inter, data->light, final, to_light);
 	return (clamp_color(final));
 }
