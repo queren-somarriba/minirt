0cfdd98428808bc45ad086ed7332f86256f80ab9
Jordan Piech <jordan.piech@gmail.com>
2025-05-27 14:48:03 +0200

    retrait de la gestion d endian de la fonction img_pix_put

--- srcs/display/display.c
+++ srcs/display/display.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   display.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qsomarri <qsomarri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jpiech <jpiech@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 19:23:40 by qsomarri          #+#    #+#             */
-/*   Updated: 2025/04/25 19:13:55 by qsomarri         ###   ########.fr       */
+/*   Updated: 2025/05/27 14:47:01 by jpiech           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,9 @@
 void	img_pix_put(t_img img, int x, int y, int color)
 {
 	char	*pixel;
-	int		i;
 
-	i = img.bpp - 8;
 	pixel = img.addr + (y * img.line_len + x * (img.bpp / 8));
-	while (i >= 0)
-	{
-		if (img.endian)
-			*pixel++ = (color >> i) & 0xFF;
-		else
-			*pixel++ = (color >> (img.bpp - 8 - i)) & 0xFF;
-		i -= 8;
-	}
+	*(int *)pixel = color;
 }
 
 int	render(t_minirt *data)
