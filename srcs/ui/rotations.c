5d00276eb87b28985de2489fbf9ee72adc87c131
Jordan Piech <jordan.piech@gmail.com>
2025-05-27 20:18:53 +0200

    norminage

--- srcs/ui/rotations.c
+++ srcs/ui/rotations.c
@@ -6,7 +6,7 @@
 /*   By: jpiech <jpiech@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 18:55:51 by qsomarri          #+#    #+#             */
-/*   Updated: 2025/05/27 20:12:18 by jpiech           ###   ########.fr       */
+/*   Updated: 2025/05/27 20:16:40 by jpiech           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,30 +29,30 @@ static void	rotate_around(t_vector *v, t_vector axis, float angle)
 		+ k.z * dot * (1.0f - cosf(angle));
 }
 
-static voidrotate_around1(t_vector *v, t_vector axis, float angle)
+static void	rotate_around1(t_vector *v, t_vector axis, float angle)
 {
-	t_vector k;
-	t_vector cross;
-	float	dot;
-	
+	t_vector	k;
+	t_vector	cross;
+	float		dot;
+
 	k = normalize_vect(axis);
 	dot = dot_product(k, *v);
 	cross = mult_vect(k, *v);
 	if (v->y * cosf(angle) + cross.y * sinf(angle)
-		+ k.y * dot * (1.0f - cosf(angle)) < 0.9f &&
-		v->y * cosf(angle) + cross.y * sinf(angle)
+		+ k.y * dot * (1.0f - cosf(angle)) < 0.9f
+		&& v->y * cosf(angle) + cross.y * sinf(angle)
 		+ k.y * dot * (1.0f - cosf(angle)) > -0.9f)
-		{
+	{
 			v->x = v->x * cosf(angle) + cross.x * sinf(angle)
 			+ k.x * dot * (1.0f - cosf(angle));
 			v->y = v->y * cosf(angle) + cross.y * sinf(angle)
 			+ k.y * dot * (1.0f - cosf(angle));
 			v->z = v->z * cosf(angle) + cross.z * sinf(angle)
 			+ k.z * dot * (1.0f - cosf(angle));
-		}
+	}
 }
 
-static voidcompute_rotation(t_minirt *data, t_vector *v, int keysym)
+static void	compute_rotation(t_minirt *data, t_vector *v, int keysym)
 {
 	if (keysym == XK_1)
 		rotate_around1(v, data->cam->right, -M_PI / 36.0f);
