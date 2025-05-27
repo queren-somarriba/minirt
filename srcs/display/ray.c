1e41ed3a15314962ac8fe13bf20516c609ff48eb
Jordan Piech <jordan.piech@gmail.com>
2025-05-27 20:13:51 +0200

    blocage de la rotation autour de l axe x

--- srcs/display/ray.c
+++ srcs/display/ray.c
@@ -6,7 +6,7 @@
 /*   By: jpiech <jpiech@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 16:17:38 by qsomarri          #+#    #+#             */
-/*   Updated: 2025/05/27 19:15:29 by jpiech           ###   ########.fr       */
+/*   Updated: 2025/05/27 20:12:52 by jpiech           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ t_vector	make_vect_dir(t_minirt *data, float x, float y)
 	f[2] = (2.0f * (x + 0.5f) / WIN_WIDTH - 1.0f) * f[0] * f[1];
 	f[3] = (1.0f - 2.0f * (y + 0.5f) / WIN_HEIGHT) * f[1];
 	data->cam->forw = normalize_vect(*data->cam->v);
-	if (fabsf(data->cam->forw.x) == 0.0f || fabsf(data->cam->forw.z) == 0.0f)
+	if (fabsf(data->cam->forw.x) == 0.0f && fabsf(data->cam->forw.z) == 0.0f)
 		data->cam->right = (t_vector){1, 0, 0};
 	else
 		data->cam->right = normalize_vect(
--- srcs/ui/rotations.c
+++ srcs/ui/rotations.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rotations.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qsomarri <qsomarri@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jpiech <jpiech@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 18:55:51 by qsomarri          #+#    #+#             */
-/*   Updated: 2025/04/25 19:16:34 by qsomarri         ###   ########.fr       */
+/*   Updated: 2025/05/27 20:12:18 by jpiech           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,35 @@ static void	rotate_around(t_vector *v, t_vector axis, float angle)
 		+ k.z * dot * (1.0f - cosf(angle));
 }
 
-static void	compute_rotation(t_minirt *data, t_vector *v, int keysym)
+static voidrotate_around1(t_vector *v, t_vector axis, float angle)
+{
+	t_vector k;
+	t_vector cross;
+	float	dot;
+	
+	k = normalize_vect(axis);
+	dot = dot_product(k, *v);
+	cross = mult_vect(k, *v);
+	if (v->y * cosf(angle) + cross.y * sinf(angle)
+		+ k.y * dot * (1.0f - cosf(angle)) < 0.9f &&
+		v->y * cosf(angle) + cross.y * sinf(angle)
+		+ k.y * dot * (1.0f - cosf(angle)) > -0.9f)
+		{
+			v->x = v->x * cosf(angle) + cross.x * sinf(angle)
+			+ k.x * dot * (1.0f - cosf(angle));
+			v->y = v->y * cosf(angle) + cross.y * sinf(angle)
+			+ k.y * dot * (1.0f - cosf(angle));
+			v->z = v->z * cosf(angle) + cross.z * sinf(angle)
+			+ k.z * dot * (1.0f - cosf(angle));
+		}
+}
+
+static voidcompute_rotation(t_minirt *data, t_vector *v, int keysym)
 {
 	if (keysym == XK_1)
-		rotate_around(v, data->cam->right, -M_PI / 36.0f);
+		rotate_around1(v, data->cam->right, -M_PI / 36.0f);
 	else if (keysym == XK_2)
-		rotate_around(v, data->cam->right, M_PI / 36.0f);
+		rotate_around1(v, data->cam->right, M_PI / 36.0f);
 	else if (keysym == XK_3)
 		rotate_around(v, data->cam->up, -M_PI / 36.0f);
 	else if (keysym == XK_4)
